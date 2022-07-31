961bdecd07383e5ee50c5be95c093864faa05618
poyingHAHA <poying.code@gmail.com>
2022-07-31 19:02:34 +0800

    Middleware3 - 中間件類，實作對request query的檢查

---
+++ Middleware1/Middleware1/CheckMIddleware.cs
@@ -0,0 +1,34 @@
+﻿using Dynamic.Json;
+
+namespace Middleware1
+{
+    public class CheckMIddleware
+    {
+        private readonly RequestDelegate next;
+
+        public CheckMIddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context) // 名字不要打錯
+        {
+            string password = context.Request.Query["password"];
+            if(password == "123")
+            {
+                if(context.Request.HasJsonContentType())
+                {
+                    var stream = context.Request.BodyReader.AsStream();
+                    // system.text.json目前不支持把Json反序列化為dynamic類型，要安裝dynamic.json
+                    dynamic? obj = DJson.ParseAsync(stream);
+                    context.Items["BodyJson"] = obj;
+                }
+                await next.Invoke(context);
+            }
+            else
+            {
+                context.Response.StatusCode = 401;
+            }
+        }
+    }
+}
--- Middleware1/Middleware1/Program.cs
+++ Middleware1/Middleware1/Program.cs
@@ -7,7 +7,8 @@ var app = builder.Build();
 
 app.Map("/test", async (pipeBuilder) =>
 {
-    // 第一個中間件，context代表當前請求上下文，next代表下一個中間件
+    pipeBuilder.UseMiddleware<CheckMIddleware>();
+
     pipeBuilder.Use(async (context, next) =>
     {
         context.Response.ContentType = "text/html";
@@ -16,15 +17,24 @@ app.Map("/test", async (pipeBuilder) =>
         await context.Response.WriteAsync("1 End<br/>");
     });
 
-    pipeBuilder.Use(async (context, next) => // 第二個中間件
+    pipeBuilder.Use(async (context, next) => 
     {
-        // 不用再設置content Type因為第一個中間件已經弄好了
         await context.Response.WriteAsync("2 Start<br/>");
         await next.Invoke();
         await context.Response.WriteAsync("2 End<br/>");
     });
 
     pipeBuilder.UseMiddleware<Test1MiddleWare>();
+    pipeBuilder.Run(async context =>
+    {
+        await context.Response.WriteAsync("Run<br/>");
+        dynamic? obj = context.Items["BodyJson"];
+        if(obj!=null)
+        {
+            await context.Response.WriteAsync($"{obj.ToString()}<br/>");
+        }
+
+    });
 
     pipeBuilder.Run(async ctx => // 最後一個中間件沒有next
     {
